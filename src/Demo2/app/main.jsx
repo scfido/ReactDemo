ddfc33238e5d425a2bb23958d02f4a5dad74f5a8
fu yun <scfido@hotmail.com>
2015-11-15 21:10:32 +0800

    增加CSS热加载示例

--- src/Demo2/app/main.jsx
+++ src/Demo2/app/main.jsx
@@ -2,6 +2,7 @@
 import ReactDOM from 'react-dom';
 import Content from './content';
 import Footer from './footer';
+import css from '../wwwroot/css/base.css';
 
 
 //注意，修改此文件不能实现热加载
--- src/Demo2/webpack.config.js
+++ src/Demo2/webpack.config.js
@@ -1,5 +1,7 @@
 ﻿var webpack = require('webpack');
 var path = require('path');
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
 module.exports = {
     devtool: 'source-map',
@@ -9,6 +11,11 @@ module.exports = {
         'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
         path.resolve(__dirname, 'app/main')
     ],
+    resolve: {
+        alias: {
+            'react': pathToReact
+        }
+    },
     output: {
         path: path.resolve(__dirname, 'wwwroot/app'),
         publicPath: "http://127.0.0.1:8080/app/",
@@ -29,8 +36,18 @@ module.exports = {
                 exclude: /node_modules/,
                 loaders: ['react-hot', 'babel'],
                 include: path.join(__dirname, 'app') 
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+                //loaders: ["style","css"]
             }
-        ]
+        ],
+        resolve: {
+            alias: {
+                'react': pathToReact
+            }
+        }
     },
     resolve: {
         extensions: ['', '.jsx', ".js",'.json']
